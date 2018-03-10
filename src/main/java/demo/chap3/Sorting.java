1f5e6958d112e012ef1222405dbba3f1c57de10d
JC <shenjianchun@gmail.com>
2018-03-10 20:54:00 +0800

    add chap3

---
+++ src/main/java/demo/chap3/Sorting.java
@@ -0,0 +1,50 @@
+package demo.chap3;
+
+import java.util.Comparator;
+
+public class Sorting {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Apple {
+        private Integer weight = 0;
+        private String color = "";
+
+        public Apple(Integer weight, String color){
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public Integer getWeight() {
+            return weight;
+        }
+
+        public void setWeight(Integer weight) {
+            this.weight = weight;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public String toString() {
+            return "Apple{" +
+                    "color='" + color + '\'' +
+                    ", weight=" + weight +
+                    '}';
+        }
+    }
+
+    static class AppleComparator implements Comparator<Apple> {
+        public int compare(Apple a1, Apple a2){
+            return a1.getWeight().compareTo(a2.getWeight());
+        }
+    }
+
+}
